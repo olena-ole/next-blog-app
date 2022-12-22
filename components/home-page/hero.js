bfd7744dcbc2ac4c5bc98b2d5b9d48d6977ccb89
Olena <pampam32121@gmail.com>
2022-12-22 14:39:28 +0200

    added Hero component

---
+++ components/home-page/hero.js
@@ -0,0 +1,21 @@
+import classes from './hero.module.css';
+import Image from 'next/image'
+
+export default function Hero() {
+    return (
+        <section className={classes.hero}>
+            <div className={classes.image}>
+                <Image 
+                    src="/images/site/olena.jpg" 
+                    alt="An image showing Olena" 
+                    width={300} 
+                    height={300}
+                />
+            </div>
+            <h1>HI, I'm Olena</h1>
+            <p>
+                I blog about web development - especially about frontend frameworks Next.js or React library
+            </p>
+        </section>
+    );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import { Fragment } from "react";
-
+import Hero from "../components/home-page/hero";
 export default function HomePage() {
     return (
         <Fragment>
             <Hero />
-            <FeaturedPosts />
+            {/* <FeaturedPosts /> */}
         </Fragment>
     )
 };
