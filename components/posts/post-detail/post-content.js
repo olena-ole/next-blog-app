b288f72f03edff2f7691c40e6093ac281c2c6c10
Olena <pampam32121@gmail.com>
2022-12-28 06:52:49 +0200

    added second post, rendered code snippets from markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,4 +1,6 @@
 import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import PostHeader from './post-header';
 import Image from 'next/image';
 import classes from './post-content.module.css';
@@ -39,8 +41,19 @@ export default function PostContent(props) {
                 )
             }
 
-            return <p>{paragraph.children}</p>
-        }
+            return <p>{paragraph.children}</p>;
+        },
+
+        code(code) {
+            const { className, children } = code;
+            const language = className.split('-')[1];
+       
+            return (
+              <SyntaxHighlighter language={language} style={atomDark}>
+                {children[0]}
+              </SyntaxHighlighter>
+            );
+        },
     };
 
     return (
