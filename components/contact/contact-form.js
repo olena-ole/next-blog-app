854dc6f00e8f4f2620288f5d6d8e6ecd0779f9a3
Olena <pampam32121@gmail.com>
2022-12-28 12:17:01 +0200

    in contact form, sent data from the client to api route (my variant)

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,25 +1,63 @@
+import { useRef } from 'react';
 import classes from './contact-form.module.css';
 
 export default function ContactForm() {
+    const emailInput = useRef(),
+        nameInput = useRef(),
+        textareaInput = useRef();
+
+
+    function sendMessageHandler(e) {
+        e.preventDefault();
+
+        const email = emailInput.current.value,
+            name = nameInput.current.value,
+            message = textareaInput.current.value;
+
+        if (
+            !email || 
+            !email.includes('@') ||
+            !name || 
+            name.trim() === '' ||
+            !message || 
+            message.trim() === ''
+        ) {
+            console.log('Invalid input');
+            return;
+        }
+
+        const newMessage = { email, name, message };
+
+        fetch('/api/contact', {
+            method: 'POST',
+            body: JSON.stringify(newMessage),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }
+
     return (
         <section className={classes.contact}>
             <h1>How Can I Help You?</h1>
 
-            <form className={classes.form}>
+            <form className={classes.form} onSubmit={sendMessageHandler}>
                 <div className={classes.controls}>
                     <div className={classes.control}>
                         <label htmlFor="email">Your Email</label>
-                        <input type="email" id="email" required/>
+                        <input type="email" id="email" required ref={emailInput}/>
                     </div>
                     <div className={classes.control}>
                         <label htmlFor="name">Your Name</label>
-                        <input type="text" id="name" required/>
+                        <input type="text" id="name" required ref={nameInput}/>
                     </div>
                 </div>
 
                 <div className={classes.control}>
                     <label htmlFor="message">Your Message</label>
-                    <textarea id="name" rows={5}/>
+                    <textarea id="name" rows={5} ref={textareaInput}/>
                 </div>
 
                 <div className={classes.actions}>
