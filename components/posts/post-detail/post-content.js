c8622c23d05c84a75fa6acf9114bd453dc8fc361
Olena <pampam32121@gmail.com>
2022-12-28 06:01:53 +0200

    rendered images with Next Image and ReactMarkdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,25 +1,52 @@
 import ReactMarkdown from 'react-markdown';
+import PostHeader from './post-header';
+import Image from 'next/image';
 import classes from './post-content.module.css';
 
-import PostHeader from './post-header';
-
-// const DUMMY_POST = { 
-//     slug: 'getting-started-with-nextjs',
-//     title:'Gettind Started with NextJS',
-//     image:'getting-started-nextjs.png',
-//     date: '2022-02-10',
-//     content: '# This is a first post'
-// };
 
 export default function PostContent(props) {
     const { post } = props;
 
     const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
+    const customRenderers = {
+        // img(image) {
+        //     return (
+        //         <Image 
+        //             src={`/images/posts/${post.slug}/${image.src}`}
+        //             alt={image.alt}
+        //             width={600}
+        //             height={300}
+        //         />
+        //     );
+        // },
+
+        p(paragraph) {
+            const { node } = paragraph;
+
+            if (node.children[0].tagName === 'img') {
+                const image = node.children[0];
+
+                return (
+                    <div className={classes.image}>
+                        <Image 
+                            src={`/images/posts/${post.slug}/${image.properties.src}`}
+                            alt={image.properties.alt}
+                            width={600}
+                            height={300}
+                        />
+                    </div>
+                )
+            }
+
+            return <p>{paragraph.children}</p>
+        }
+    };
+
     return (
         <article className={classes.content}>
             <PostHeader title={post.title} image={imagePath} />
-            <ReactMarkdown>{post.content}</ReactMarkdown>
+            <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
         </article>
     );
 };
