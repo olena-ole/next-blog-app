692187cbbdb4fc38e53e330d3f2c3b2add5aa640
Olena <pampam32121@gmail.com>
2022-12-30 00:15:25 +0200

    created different variables for production and development for db

--- next.config.js
+++ next.config.js
@@ -1,8 +1,23 @@
-module.exports = {
-    env: {
-        mongodb_username: 'pampam',
-        mongodb_password: 'dZ5qAusaempN',
-        mongodb_clustername: 'cluster0',
-        mongodb_database: 'my-site'
-    }
-}
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
+module.exports = (phase) => {
+    if (phase === PHASE_DEVELOPMENT_SERVER) {
+        return {
+            env: {
+                mongodb_username: 'pampam',
+                mongodb_password: 'dZ5qAusaempN',
+                mongodb_clustername: 'cluster0',
+                mongodb_database: 'my-site-dev' // folder (collection) for development
+            }
+        };
+    };
+
+    return {
+        env: {
+            mongodb_username: 'pampam',
+            mongodb_password: 'dZ5qAusaempN',
+            mongodb_clustername: 'cluster0',
+            mongodb_database: 'my-site'        // folder (collection) for production
+        }
+    } ;
+};
