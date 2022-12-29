43a11a13bc0f4e4fe98c41d1f25df803d40e7808
Olena <pampam32121@gmail.com>
2022-12-29 23:58:29 +0200

    added environment variables in next.config.js to use in api routes to connect to db

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    env: {
+        mongodb_username: 'pampam',
+        mongodb_password: 'dZ5qAusaempN',
+        mongodb_clustername: 'cluster0',
+        mongodb_database: 'my-site'
+    }
+}
